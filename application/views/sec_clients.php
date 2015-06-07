a61132cefada6009f7f1a067953af43ea0f4b875
Juan A. Delgado <juandel@juandel.local>
2015-06-06 21:43:17 -0300

    Clients - Added new clients, logos and links

--- application/views/sec_clients.php
+++ application/views/sec_clients.php
@@ -2,18 +2,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-6">
-                <a href="#">
-                    <img src="<?=base_url();?>img/logos/envato.jpg" class="img-responsive img-centered" alt="">
+                <a href="https://www.macro.com.ar" target="_blank">
+                    <img src="<?=base_url();?>img/logos/macro.png" class="img-responsive img-centered" alt="">
                 </a>
             </div>
             <div class="col-md-3 col-sm-6">
                 <a href="#">
-                    <img src="<?=base_url();?>img/logos/designmodo.jpg" class="img-responsive img-centered" alt="">
+                    <img src="<?=base_url();?>img/logos/ivi_logo.png" class="img-responsive img-centered" alt="">
                 </a>
             </div>
             <div class="col-md-3 col-sm-6">
-                <a href="#">
-                    <img src="<?=base_url();?>img/logos/themeforest.jpg" class="img-responsive img-centered" alt="">
+                <a href="http://www.superplano.com" target="_blank">
+                    <img src="<?=base_url();?>img/logos/superplano_logo.png" class="img-responsive img-centered" alt="">
                 </a>
             </div>
             <div class="col-md-3 col-sm-6">
