3803b7e4451b84ebd9da56e05a81da9bdd1f263b
Juan A. Delgado <juandel@juandel.local>
2015-06-20 17:00:45 -0300

    TEAM - added andy to the team

--- application/views/sec_team.php
+++ application/views/sec_team.php
@@ -7,14 +7,12 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                 <div class="team-member">
                     <img src="<?=base_url()?>img/team/1.jpg" class="img-responsive img-circle" alt="">
                     <h4>Juan Delgado</h4>
                     <p class="text-muted">Lead Designer</p>
                     <ul class="list-inline social-buttons">
-                        <li><a href="https://twitter.com/juddel1"><i class="fa fa-twitter"></i></a>
-                        </li>
                         <li><a href="https://www.facebook.com/juandel1984"><i class="fa fa-facebook"></i></a>
                         </li>
                         <li><a href="https://ar.linkedin.com/in/juandel"><i class="fa fa-linkedin"></i></a>
@@ -22,22 +20,20 @@
                     </ul>
                 </div>
             </div>
-            <!-- <div class="col-sm-4">
+            <div class="col-sm-6">
                 <div class="team-member">
                     <img src="<?=base_url()?>img/team/2.jpg" class="img-responsive img-circle" alt="">
-                    <h4>Larry Parker</h4>
-                    <p class="text-muted">Senior Designer</p>
+                    <h4>Andres Jaime</h4>
+                    <p class="text-muted">Architect</p>
                     <ul class="list-inline social-buttons">
-                        <li><a href="#"><i class="fa fa-twitter"></i></a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-facebook"></i></a>
+                        <li><a href="https://www.facebook.com/andres.jaime.52?fref=ts"><i class="fa fa-facebook"></i></a>
                         </li>
                         <li><a href="#"><i class="fa fa-linkedin"></i></a>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div class="col-sm-4">
+            <!-- <div class="col-sm-4">
                 <div class="team-member">
                     <img src="<?=base_url()?>img/team/3.jpg" class="img-responsive img-circle" alt="">
                     <h4>Diana Pertersen</h4>
@@ -51,8 +47,8 @@
                         </li>
                     </ul>
                 </div>
-            </div>
-        </div> -->
+            </div> -->
+        </div>
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 text-center">
                 <p class="large text-muted"></p>
