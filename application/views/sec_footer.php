73c0afdd856258aea583b0d255bc35149cf8ebf5
Juan A. Delgado <juandel@juandel.fibertel.com.ar>
2015-08-26 18:02:03 -0300

    FOOTER - Adds company phone number

--- application/views/sec_footer.php
+++ application/views/sec_footer.php
@@ -8,23 +8,25 @@
                 <ul class="list-inline social-buttons">
                     <li><a href="#"><i class="fa fa-twitter"></i></a>
                     </li>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a>
+                    <li><a href="https://www.facebook.com/jaddel.arch"><i class="fa fa-facebook"></i></a>
                     </li>
                     <li><a href="#"><i class="fa fa-linkedin"></i></a>
                     </li>
                 </ul>
             </div>
-             <!--
-             <div class="col-md-4">
-
-                <ul class="list-inline quicklinks">
+             
+             <div class="col-md-4" style="text-align:right;">
+                <span>Miami: +1 (786) 375-8039</span>
+                <br>
+                <span>Buenos Aires: +54 9 (113) 069-5826</span>
+                <!-- <ul class="list-inline quicklinks">
                     <li><a href="#">Privacy Policy</a>
                     </li>
                     <li><a href="#">Terms of Use</a>
                     </li>
-                </ul> 
+                </ul>  -->
             </div> 
-            -->
+            
         </div>
     </div>
 </footer>
