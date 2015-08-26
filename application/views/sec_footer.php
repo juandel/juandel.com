3ba03311f1b3b9a83d701005d51ca6ed24f8e792
Juan A. Delgado <juandel@juandel.fibertel.com.ar>
2015-08-26 19:51:20 -0300

    FOOTER - ADDS ADDRESS

--- application/views/sec_footer.php
+++ application/views/sec_footer.php
@@ -1,8 +1,11 @@
 <footer>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-4">
-                <span class="copyright">Copyright &copy; Jaddel - Architectural Visualization 2014</span>
+            <div class="col-md-4" style="text-align:left;padding-left:30px;">
+                <span class="copyright">300 Three Islands blvd. </span>
+                <br>
+                <span class="copyright">Hallandale, Florida
+                33009</span>
             </div>
             <div class="col-md-4">
                 <ul class="list-inline social-buttons">
@@ -10,23 +13,27 @@
                     </li>
                     <li><a href="https://www.facebook.com/jaddel.arch"><i class="fa fa-facebook"></i></a>
                     </li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                    <li><a href=""><i class="fa fa-linkedin"></i></a>
                     </li>
                 </ul>
             </div>
              
-             <div class="col-md-4" style="text-align:right;">
-                <span>Miami: +1 (786) 375-8039</span>
+             <div class="col-md-4" style="text-align:right;padding-right:30px;">
+                <!-- <span>Miami: +1 (786) 375-8039</span>
                 <br>
                 <span>Buenos Aires: +54 9 (113) 069-5826</span>
-                <!-- <ul class="list-inline quicklinks">
-                    <li><a href="#">Privacy Policy</a>
+ -->                <ul class="list-inline quicklinks">
+                    <li><span>Miami: +1 (786) 375-8039</span>
                     </li>
-                    <li><a href="#">Terms of Use</a>
+                    <li><span>Buenos Aires: +54 9 (113) 069-5826</span>
                     </li>
-                </ul>  -->
+                </ul> 
             </div> 
-            
+        </div>
+        <div class="row">
+            <div class="col-md-offset-4 col-md-4" style="margin-top:20px;">
+                <span class="copyright" >Copyright &copy; Jaddel - Architectural Visualization 2014</span>
+            </div>
         </div>
     </div>
 </footer>
